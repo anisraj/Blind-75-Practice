2d9495edf44c682162f13230f8aa05d6196ea13b
anisraj <anisjamadar26@gmail.com>
2025-12-16 10:39:45 +0530

    same tree

---
+++ Blind-75-Practice/Blind-75-Practice/src/binary_tree/SameTree.kt
@@ -0,0 +1,10 @@
+package binary_tree
+
+fun isSameTree(p: TreeNode?, q: TreeNode?): Boolean {
+    if (p == null && q == null) {
+        return true
+    }
+    return p?.`val` == q?.`val` &&
+            isSameTree(p?.left, q?.left) &&
+            isSameTree(p?.right, q?.right)
+}
